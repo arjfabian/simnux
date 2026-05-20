4b3c821f2c60b2cf9c353e56789f783629f7cdc8
jfabian <jfabian@datorum.net>
2026-05-20 16:36:15 -0300

    refactor: remove legacy kernel implementation

--- backend/kernel/commands/pwd.py
+++
@@ -1,8 +0,0 @@
-from typing import List
-from kernel.command import SimnuxCommand
-
-class Command(SimnuxCommand):
-    name = "pwd"
-
-    def execute(self, args: List[str]) -> str:
-        return self.session.current_path
